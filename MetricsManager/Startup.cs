41bea403b39e688f6ccf6d7d82b3004c744d0663
istchanar <istchanar@outlook.com>
2021-09-09 22:49:23 +0700

    lesson2.fix2

--- MetricsManager/Startup.cs
+++ MetricsManager/Startup.cs
@@ -31,18 +31,16 @@ namespace MetricsManager
             services.AddControllers();
             services.AddSingleton<AgentsFolder>();
 
-            /*
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MetricsManager", Version = "v1" });
             });
-            */
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            /*
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
